e98e76b5d596dae0e8fb153a7a66ffa87b087cc8
GuillaumeDonzeau <gdonzeau@hotmail.com>
2021-09-01 23:12:30 +0200

    Travail sur les tests CoreData en cours.

--- Reciplease/RecipleaseTests/RecipleaseStorageTests.swift
+++ Reciplease/RecipleaseTests/RecipleaseStorageTests.swift
@@ -10,8 +10,28 @@ import CoreData
 @testable import Reciplease
 
 
+class TestCoreDataStack: NSObject {
+    lazy var persistentContainer: NSPersistentContainer = {
+        let description = NSPersistentStoreDescription()
+        description.url = URL(fileURLWithPath: "/dev/null")
+        let container = NSPersistentContainer(name: "Storage Recipes")
+        container.persistentStoreDescriptions = [description]
+        container.loadPersistentStores { _, error in
+            if let error = error as NSError? {
+                fatalError("Unresolved error \(error), \(error.userInfo)")
+            }
+        }
+        return container
+    }()
+    
+}
+
 class RecipleaseStorageTests: XCTestCase {
+    let recipeCoreDataManager = RecipeCoreDataManager()
+    var recipe: Recipe?
+    
     override func setUp() {
+        
         let persistentStoreDescription = NSPersistentStoreDescription()
             persistentStoreDescription.type = NSInMemoryStoreType
         
@@ -19,10 +39,45 @@ class RecipleaseStorageTests: XCTestCase {
         
         container.persistentStoreDescriptions = [persistentStoreDescription]
         container.loadPersistentStores { (_, error) in
-            if let error = error as? NSError {
+            if let error = error as NSError? {
                 fatalError("Unresolved error \(error), \(error.userInfo)")
             }
         }
-        persistentContainer = container
+        RecipeCoreDataManager.init(persistentContainer: container)
+        //persistentContainer = container
     }
+    
+    func testToTry() {
+        
+        let recipeEntityOne:RecipeEntity
+        //recipeEntityOne.name = "Chicken"
+        recipeEntityOne.name = "Chicken"
+        recipeEntityOne.imageUrl = "www.image.com"
+        recipeEntityOne.url = "www.url.com"
+        recipeEntityOne.person = 1.00
+        recipeEntityOne.totalTime = 36.0
+        recipeEntityOne.ingredients = (["Chicken","Salt"])
+        //recipeEntityOne.name = "ChickenCesar"
+        let recipeOne = recipe(from: recipeEntityOne)
+        
+        let recipeEntityTwo = RecipeEntity()
+        recipeEntityTwo.name = "Salad Lemon"
+        let recipeTwo = Recipe(from: recipeEntityTwo)
+        
+        let recipeEntityThree = RecipeEntity()
+        recipeEntityThree.name = "Ice Cream"
+        let recipeThree = Recipe(from: recipeEntityThree)
+        
+        let context = TestCoreDataStack().persistentContainer.newBackgroundContext()
+            expectation(forNotification: .NSManagedObjectContextDidSave, object: context) { _ in
+                return true
+            }
+        let recipes:[Recipe] = try! recipeCoreDataManager.loadRecipes()
+        XCTAssert(recipes.count == 0)
+        recipeCoreDataManager.saveRecipe(recipe: recipeOne)
+        recipeCoreDataManager.saveRecipe(recipe: recipeTwo)
+        recipeCoreDataManager.saveRecipe(recipe: recipeThree)
+    }
+    
 }
+
